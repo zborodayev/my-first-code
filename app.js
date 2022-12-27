945e17f016b6862634311c25bdee38e6be682924
Zakhar Borodayev <zborodayev@gmail.com>
2022-12-28 00:49:51 +0600

    make new variable

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-// базовые орефметик операторы
+
 const whith = 10
 const height = 20
 const space =  whith * height
-console.log(space)
+console.log(`perimeter: ${space}`)
+const a = 10
+const b = 30
+const c = 40
+const d = a * b * c
+console.log(`square: ${d}`)
