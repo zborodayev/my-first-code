813ac43300688e9ae9d2b6ac3c9026b6ae278286
Zakhar Borodayev <zborodayev@gmail.com>
2022-12-28 20:21:04 +0600

    big rework and adding an explanation

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// basic arithmetic operations
+console.log('hi, it is my frist code in js! let see it!');
+console.log('basic arithmetic operations');
 const w = 10;
 const h = 5;
 const s = w * h;
@@ -5,4 +8,19 @@ const nw = w - 4;
 const nw2 = w + 4;
 const d = nw / nw2;
 console.log(`10 - 4 / 10 + 4 =`,d);
+a1 = 10;
+a4 = a1 ** 3;
+console.log(`a = 10, a ** 3 = a * a * a, and a ** 3 =`,a4);
+// lines
+console.log('lines');
+a = 'hello';
+b = 'world';
+c = a + ' ' + b;
+console.log(`hello' + ' ' + 'world =`,c);
+// assignment operators 
+console.log('assignment operators');
+age = 25 + 4
+age += 5
+console.log(`age = 25 + 4, age += 5 == age = age + 5, age =`,age);
+
 
