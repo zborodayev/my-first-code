cedd340990507fd28809ba054d530cb8c1be2032
Zakhar Borodayev <zborodayev@gmail.com>
2022-12-28 20:43:12 +0600

    rework console

--- app.js
+++ app.js
@@ -19,8 +19,25 @@ c = a + ' ' + b;
 console.log(`hello' + ' ' + 'world =`,c);
 // assignment operators 
 console.log('assignment operators');
-age = 25 + 4
-age += 5
-console.log(`age = 25 + 4, age += 5 == age = age + 5, age =`,age);
-
-
+age = 25 + 4;
+ag1 = age += 1;
+ag2 = age -= 3;
+ag3 = age *= 3;
+ag4 = age /= 4;
+ag5 = age ++;
+ag6 = age -- ;
+console.log(`age = 29`);
+console.log(`age += 1 == age = age + 1 =`,ag1);
+console.log(`age -= 3 == age = age - 3 =`,ag2);
+console.log(`age *= 3 == age = age * 3 =`,ag3);
+console.log(`age /= 4 == age = age / 4 =`,ag4);
+console.log(`age ++ = age = age + 1 =`,ag5);
+console.log(`age -- = age = age - 1 =`,ag6);
+// comparison
+console.log('comparison');
+b = 10;
+c = 20;
+b2 = b > c;
+c2 = c > b;
+console.log(`10 > 20? true or false?`,b2);
+console.log(`20 > 10? true or false?`,c2);
